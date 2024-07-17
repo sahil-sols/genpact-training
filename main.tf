7e1d9c82962022e3956512a65fe7822fa0ad20ac
sahil@solsphere.ai <sahilpugalia@Sahils-MacBook-Pro.local>
2024-07-17 15:08:31 +0530

    change default nodepool to 1

--- main.tf
+++ main.tf
@@ -23,7 +23,7 @@ resource "azurerm_kubernetes_cluster" "cluster" {
 
   default_node_pool {
     name       = "default"
-    node_count = "2"
+    node_count = "1"
     vm_size    = "standard_d2_v2"
   }
 
